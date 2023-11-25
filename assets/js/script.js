6b1dcfd3c5f6b8d1609831aa3a70225e908ccb4d
Nelson <exio78@hotmail.com>
2023-11-25 20:30:32 -0300

    Changed bottom bar color and added a script that toggles bottom bar arrow direction

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const toggleBottomBarArrow = () => {
+    const bottomBarArrow = document.querySelector('#bottomBarArrow');
+    bottomBarArrow.classList.toggle('fa-arrow-up');
+    bottomBarArrow.classList.toggle('fa-arrow-down');
+}
